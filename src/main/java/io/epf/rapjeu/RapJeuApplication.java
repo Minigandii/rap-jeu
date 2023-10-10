dd5c7f45a582de5622331d496b17f3f449278399
Lucas BARREAU <65079955+PatLaCisay@users.noreply.github.com>
2023-10-10 11:25:36 +0200

    .gitignore

---
+++ src/main/java/io/epf/rapjeu/RapJeuApplication.java
@@ -0,0 +1,13 @@
+package io.epf.rapjeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapJeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapJeuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/epf/rapjeu/RapJeuApplicationTests.java
@@ -0,0 +1,13 @@
+package io.epf.rapjeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapJeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
