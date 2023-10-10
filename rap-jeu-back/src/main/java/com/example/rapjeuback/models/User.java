0bff7d59fba579b9ca446e5f471aeb9195c42299
Lucas BARREAU <65079955+PatLaCisay@users.noreply.github.com>
2023-10-10 15:01:36 +0200

    pom with entity

---
+++ rap-jeu-back/src/main/java/com/example/rapjeuback/models/User.java
@@ -0,0 +1,13 @@
+package com.example.rapjeuback.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@Getter
+
+public class User {
+}
