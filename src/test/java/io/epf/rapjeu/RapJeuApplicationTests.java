ed6cd237749de72b5dc161bc684cef181a7c3204
Lucas BARREAU <65079955+PatLaCisay@users.noreply.github.com>
2023-10-10 11:26:04 +0200

    initial commit

--- src/test/java/io/epf/rapjeu/RapJeuApplicationTests.java
+++ src/test/java/io/epf/rapjeu/RapJeuApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RapJeuApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
